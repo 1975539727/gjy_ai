3cb9d63d9d15754511e969e0c182f7f199ba33ca
1975539727 <1975539727@qq.com>
2025-05-29 21:32:51 +0800

    红包算法和知识库

--- algorithm/big_num_add/4.js
+++ algorithm/big_num_add/4.js
@@ -1,4 +1,6 @@
 const bigNum=123456789012345678901234567890123456789n
-const theNum=new BigInt("123456789012345678901234567890123456789")
+// BigInt  申明方式 函数申明
+// bigint 简单数据类型,不是对象，不是构造函数，不能new
+const theNum=new BigInt("123456789012345678901234 567890123456789")
 console.log(bigNum,theNum,typeof bigNum,typeof 1);
 console.log(bigNum+1n)

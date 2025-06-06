a4dd04016086d3062aaf1da1d7156afa1820f875
1975539727 <1975539727@qq.com>
2025-06-06 17:07:04 +0800

    JS原型与原型链

---
+++ batjtmd/js/map/1.js
@@ -0,0 +1,4 @@
+// NaN Not a Number
+// parseInt 将字符串转化为整数  JS 前端场景 
+// input 输入 ->Number 
+console.log(['1','2','3'].map(parseInt));
--- webllm/js/script.js
+++
@@ -1,2 +0,0 @@
-// 这里可以添加JavaScript代码
-console.log('WebLLM项目已启动');

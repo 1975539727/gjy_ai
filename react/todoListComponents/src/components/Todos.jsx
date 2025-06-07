0b76144e3488574e0c82d53cc3111001e710ba32
1975539727 <1975539727@qq.com>
2025-06-07 17:08:21 +0800

    react 数据绑定和数据驱动

--- react/todoListComponents/src/components/Todos.jsx
+++ react/todoListComponents/src/components/Todos.jsx
@@ -1,6 +1,21 @@
-function Todos() {
+// 列表的渲染
+function Todos(props) {
+    // 父组件传过来的数据呢？ 传参 
+    console.log(props,'////');
+    const todos = props.todos; 
     return(
-        <ul></ul>
+        <ul>
+           {
+                // 当下这个位置 
+                // 数据为王  界面是被驱动的
+                // 数据驱动
+                // 数据绑定 data binding 
+                // 发生改变后 自动刷新界面
+                todos.map(todo=>(
+                    <li key={todo.id}>{todo.text}</li>
+                ))
+            }
+        </ul>
     )
 }
 export default Todos;

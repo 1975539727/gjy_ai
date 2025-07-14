acf7276cc91ca34cab1b5dbfec57308f516ba7e2
1975539727 <1975539727@qq.com>
2025-07-14 21:41:31 +0800

    route new 路由守卫

---
+++ react/deep-router/src/pages/Login/index.jsx
@@ -0,0 +1,46 @@
+import {
+    useState
+} from 'react'
+import {
+    useNavigate, // Navigationv组件 js跳转
+    useLocation
+} from'react-router-dom'
+const login=()=>{
+    const location=useLocation();// 
+    // console.log(location.state.form);
+    const navigate =useNavigate();// navigate 能力 
+    const [username,setUsername]=useState('')
+    const [password,setPassword]=useState('')
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        // console.log(username,password)
+        if(username==='admin'&&password==='123456'){
+            localStorage.setItem('isLogin',true)
+            alert('登录成功')
+            navigate(location?.state?.form|| '/');
+        }else{
+            alert('用户名或密码错误')
+        }
+      }
+     return (
+        <form onSubmit={handleSubmit}>
+           <h1>登录页面</h1>
+           <input 
+           type="text" 
+           placeholder="请输入用户名"
+           required
+           value={username}
+           onChange={(e)=>{setUsername(e.target.value)}}
+           />
+           <input 
+           type="password"
+           placeholder="请输入密码"
+           required
+           value={password}
+           onChange={(e)=>{setPassword(e.target.value)}}
+           />
+           <button type="submit">登录</button>
+        </form>
+     )
+}
+export default login

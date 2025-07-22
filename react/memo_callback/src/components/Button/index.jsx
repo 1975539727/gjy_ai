7a9b6c349d3e9b10c6a076585d291d4df7c1c225
1975539727 <1975539727@qq.com>
2025-07-22 12:01:26 +0800

    useMemo and useCallback  react transition

---
+++ react/memo_callback/src/components/Button/index.jsx
@@ -0,0 +1,13 @@
+import {
+    useEffect,
+    memo
+} from 'react'
+const Button =()=>{
+    useEffect(()=>{
+        console.log('Button useEffect')
+    },[])
+    console.log('Button render')
+  return <button>Click Me</button>
+}
+// 高阶组件 
+export default memo(Button)
